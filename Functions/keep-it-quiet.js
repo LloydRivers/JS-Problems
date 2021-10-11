5dab9ed2405f76a05e900eebdd91b0949426a7ea
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-11 10:54:34 +0100

    First commit

---
+++ Functions/keep-it-quiet.js
@@ -0,0 +1,9 @@
+// Write a function `keepItQuiet` that accepts a string as an argument. The function should return the
+// lowercase version of the string with 3 periods added to the end of it.
+function keepItQuiet(str){
+    return str.toLowerCase() + "..."
+}
+
+console.log(keepItQuiet("HOORAY")); // 'hooray...'
+console.log(keepItQuiet("Doggo")); // 'doggo...'
+console.log(keepItQuiet("WHAT?!?!")); // 'what?!?!...'
