95600320410e1d444d2df170e5046089ccc885f0
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-11 15:15:50 +0100

    .repeat() used. Solved in 1 line

---
+++ CodeWars/8KYU/strRepeat.js
@@ -0,0 +1,5 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+let repeatStr = (num, str) => str.repeat(num);
+
+console.log(repeatStr(6, "I"))
+console.log(repeatStr(5, "Hello"))
