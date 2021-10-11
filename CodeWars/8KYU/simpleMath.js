3b24efb30d6792099afef0b00da0be2ec5569a0c
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-11 15:42:52 +0100

    3 different solutions to the same problem.

---
+++ CodeWars/8KYU/simpleMath.js
@@ -0,0 +1,20 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+
+const simpleMultiplication = num => num % 2 === 0 ? num*=8 : num*=9;
+
+
+// function simpleMultiplication(num) {
+//    return num % 2 === 0 ? num*=8 : num*=9
+// }
+
+
+// function simpleMultiplication(num) {
+//     if(num % 2 === 0){
+//         return num*=8
+//     } else {
+//         return num*=9
+//     }
+// }
+
