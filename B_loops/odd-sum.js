a4b2e938fda426a65779e7fccce2df4dc8c0f0c5
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-12 20:37:43 +0100

    b Loops

---
+++ B_loops/odd-sum.js
@@ -0,0 +1,16 @@
+// Write a function named `oddSum(max)` that accepts a max number as an argument. The function should
+// return the total sum of all odd numbers from 1 to the max, inclusive.
+//
+// For example, oddSum(10) should return 25 because 1 + 3 + 5 + 7 + 9 = 25
+function oddSum(max){
+    let total = 0;
+    for (let i = 0; i<=max; i++) {
+        if( i%2!==0) {
+            total += i
+        }
+    }
+    return total
+}
+
+console.log(oddSum(10)); // 25
+console.log(oddSum(5)); // 9
---
+++ B_loops/sum-up-to.js
@@ -0,0 +1,15 @@
+// Write a function named `sumUpTo(max)` that accepts a max number as an argument. The function should
+// return the total sum of all whole numbers from 1 to the max, inclusive.
+//
+// For example, sumUpTo(4) should return 10 because 1 + 2 + 3 + 4 = 10.
+function sumUpTo(max) {
+    let total = 0;
+    for (let i = 0; i<=max; i++) {
+        total +=i
+    }
+    return total
+}
+
+console.log(sumUpTo(4)); // 10
+console.log(sumUpTo(5)); // 15
+console.log(sumUpTo(2)); // 3
