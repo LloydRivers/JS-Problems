b86e460bf2a4fa5bd2e511c85261295359db70b8
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-14 08:57:11 +0100

    Arrays: longest word

---
+++ C_arrays/lengthiest-word.js
@@ -0,0 +1,20 @@
+// Write a function `lengthiestWord` that accepts a sentence string as an argument. The function should
+// return the longest word of the sentence. If there is a tie, return the word that appears later 
+// in the sentence.
+
+function lengthiestWord(str) {
+    let words = str.split(' ');
+    let first = words[0];
+    // console.log(first)
+    for (let i = 1; i < words.length; i++) {
+       if (words[i].length > first.length ) {
+          first = words[i]
+       }
+    }
+    return first
+}
+
+console.log(lengthiestWord("I am pretty hungry")); // 'hungry'
+console.log(lengthiestWord("we should think outside of the box")); // 'outside'
+console.log(lengthiestWord("down the rabbit hole")); // 'rabbit'
+console.log(lengthiestWord("simmer down")); // 'simmer'
