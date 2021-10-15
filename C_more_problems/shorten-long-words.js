6d081afb5ec1547337368a2720dbda7fcdeec33f
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-15 10:35:58 +0100

    C problems solved

--- C_more_problems/shorten-long-words.js
+++ C_more_problems/shorten-long-words.js
@@ -1,7 +1,27 @@
 // Write a function `shortenLongWords` that accepts a sentence string as an argument. The function
 // should return the same sentence where words longer than 4 characters have their vowels removed.
 function shortenLongWords(str) {
-   
+  let words = str.split(" ");
+  let array = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      let changeWord = replaceVowel(words[i]);
+      array.push(changeWord);
+    } else {
+      array.push(words[i]);
+    }
+  }
+  return array.join(' ');
+}
+function replaceVowel(word) {
+  let sentence = "";
+  let vowels = "a,e,i,o,u";
+  for (let i = 0; i < word.length; i++) {
+    if (!vowels.includes(word[i])) {
+      sentence += word[i];
+    }
+  }
+  return sentence;
 }
 
 console.log(shortenLongWords("they are very noble people")); // 'they are very nbl ppl'
