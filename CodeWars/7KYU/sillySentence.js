2f495eb7af058295bde0f2b5c01c1c628278e2f4
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-10-16 09:45:51 +0100

    silly sentence

---
+++ CodeWars/7KYU/sillySentence.js
@@ -0,0 +1,8 @@
+
+// Write a function, sillySentence(), that has 3 string parameters and returns the following silly sentence with the blanks filled in by the arguments passed into the function:
+
+const sillySentence = (a,b,c) => {
+    return `I am so ${a} because I ${b} coding! Time to write some more awesome ${c}!`
+}
+
+console.log(sillySentence('excited', 'love', 'functions')) 
