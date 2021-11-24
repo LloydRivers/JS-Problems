d91fe316eb63fe543529c4cbaec6ff62ec45e2f8
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-11-24 20:54:45 +0000

    minimum values

---
+++ Eloquent-JS/Functions/index.js
@@ -0,0 +1,7 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(a, b){
+    return Math.min(a,b)
+  }
+  console.log(min(0, 10));
+  console.log(min(0, -10));
