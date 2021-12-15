a30cfe1977290609785596742d4619b906626b44
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-12-15 13:10:09 +0000

    Objects

---
+++ 30-Days-Of-Javascript/Objects/one.js
@@ -0,0 +1,21 @@
+// Exercises: Level 1
+// Create an empty object called dog
+// Print the the dog object on the console
+// Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+// Get name, legs, color, age and bark value from the dog object
+// Set new properties the dog object: breed, getDogInfo
+const dog = {
+name: "spot",
+legs: 4,
+color: "black",
+age: 10,
+bark(){
+   return ("woof")
+}
+}
+console.log(dog.name);
+console.log(dog.legs);
+console.log(dog.color);
+console.log(dog.bark());
+
+
